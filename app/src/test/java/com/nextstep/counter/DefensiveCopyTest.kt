52899e5b3f713f08069fdeb29897f177022d7718
olaf-prnd <olaf@prnd.co.kr>
2022-06-02 21:02:33 +0900

    MutableList를 이용한 방어적 복사 테스트 케이스 추가

---
+++ app/src/test/java/com/nextstep/counter/DefensiveCopyTest.kt
@@ -0,0 +1,22 @@
+package com.nextstep.counter
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class DefensiveCopyTest {
+
+    @Test
+    fun `2개의 리스트는 서로 값이 공유되지 않아야한다`() {
+        // given
+        val list1: MutableList<Int> = mutableListOf(1, 2, 3)
+
+        // when
+        val list2: List<Int> = list1
+        list1.add(4)
+
+        // then
+        assertThat(list1).isNotEqualTo(list2)
+        assertThat(list1).containsExactly(1, 2, 3, 4)
+        assertThat(list2).containsExactly(1, 2, 3)
+    }
+}
