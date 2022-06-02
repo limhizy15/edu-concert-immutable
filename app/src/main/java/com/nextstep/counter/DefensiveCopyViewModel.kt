40921423fbc22193814bc2a6842045cab2dc6b23
Suhyeon Kim <wisemuji@gmail.com>
2022-06-02 21:27:49 +0900

    feat: 방어적 복사 케이스를 위한 DefensiveCopyViewModel 추가

---
+++ app/src/main/java/com/nextstep/counter/DefensiveCopyViewModel.kt
@@ -0,0 +1,16 @@
+package com.nextstep.counter
+
+import androidx.lifecycle.ViewModel
+
+
+class DefensiveCopyViewModel : ViewModel() {
+
+    private val _users: MutableList<User> = mutableListOf()
+    val users: List<User>
+        get() = _users
+
+    fun fetchUsers() {
+        _users.add(User(name = "홍길동", age = 10))
+        _users.add(User(name = "김길동", age = 11))
+    }
+}
