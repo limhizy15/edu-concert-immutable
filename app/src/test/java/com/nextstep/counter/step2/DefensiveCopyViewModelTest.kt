5f8bd94529d321647e02d7427cdb8ccc972e03de
galcyurio <galcyurio@gmail.com>
2022-06-02 21:37:29 +0900

    외부에서 리스트를 변경하는 테스트 케이스 추가

--- app/src/test/java/com/nextstep/counter/step2/DefensiveCopyViewModelTest.kt
+++ app/src/test/java/com/nextstep/counter/step2/DefensiveCopyViewModelTest.kt
@@ -2,6 +2,7 @@ package com.nextstep.counter.step2
 
 import com.google.common.truth.Truth.assertThat
 import com.nextstep.counter.DefensiveCopyViewModel
+import com.nextstep.counter.User
 import org.junit.Test
 
 
@@ -21,4 +22,19 @@ class DefensiveCopyViewModelTest {
         assertThat(viewModel.users).isNotEqualTo(originalUsers)
     }
 
+    @Test
+    fun `외부에서 리스트를 변경해도 내부의 리스트는 수정되지 않아야한다`() {
+        // given
+        viewModel.fetchUsers()
+
+        // when
+        val externalList = viewModel.users as MutableList<User>
+        externalList.clear()
+
+        // then
+        val internalList = viewModel.users
+        assertThat(internalList).isNotEqualTo(externalList)
+        assertThat(internalList).isNotEmpty()
+        assertThat(externalList).isEmpty()
+    }
 }
