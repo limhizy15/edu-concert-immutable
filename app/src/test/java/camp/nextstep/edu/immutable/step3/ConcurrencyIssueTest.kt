39890381fc53ee6742549e6029400b90c9e16ebf
TaeHyun Kim <galcyurio@gmail.com>
2022-06-06 19:51:08 +0900

    동시성 이슈 실습 코드 작성

---
+++ app/src/test/java/camp/nextstep/edu/immutable/step3/ConcurrencyIssueTest.kt
@@ -0,0 +1,43 @@
+package camp.nextstep.edu.immutable.step3
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import kotlin.concurrent.thread
+import kotlin.random.Random
+
+data class RandomInt(var value: Int)
+
+class ConcurrencyIssueTest {
+    private val randomInt = RandomInt(0)
+
+    @Test
+    fun `TODO`() {
+        changeRandomInt(3000L)
+        assertRandomInt(3000L)
+    }
+
+    private fun changeRandomInt(timeout: Long) {
+        startTimeoutThread(timeout) {
+            Thread.sleep(100L)
+            randomInt.value = Random.nextInt(1, 100)
+        }
+    }
+
+    private fun assertRandomInt(timeout: Long) {
+        startTimeoutThread(timeout) {
+            Thread.sleep(70L)
+            if (randomInt.value > 50) return@startTimeoutThread // 100
+            assertThat(randomInt.value).isAtMost(50)
+        }
+    }
+
+    private fun startTimeoutThread(timeout: Long, f: () -> Unit) {
+        val current = System.currentTimeMillis()
+        val expireAt = current + timeout
+        thread {
+            while (current < expireAt) {
+                f()
+            }
+        }
+    }
+}
