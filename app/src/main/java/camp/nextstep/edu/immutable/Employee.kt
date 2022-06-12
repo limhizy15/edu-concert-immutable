31ca115967b8320a1a082c797972ccc242fc7c23
Suhyeon Kim <wisemuji@gmail.com>
2022-06-12 14:40:21 +0900

    EmployeeDatabase를 통해 사원 초기화

--- app/src/main/java/camp/nextstep/edu/immutable/Employee.kt
+++ app/src/main/java/camp/nextstep/edu/immutable/Employee.kt
@@ -7,4 +7,32 @@ class Employee {
     var salary: Int? = null
     var unusedVacationCount: Int = 0
     var usedVacations: MutableList<LocalDateTime>? = null
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Employee
+
+        if (name != other.name) return false
+        if (salary != other.salary) return false
+        if (unusedVacationCount != other.unusedVacationCount) return false
+        if (usedVacations != other.usedVacations) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name?.hashCode() ?: 0
+        result = 31 * result + (salary ?: 0)
+        result = 31 * result + unusedVacationCount
+        result = 31 * result + (usedVacations?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String {
+        return "Employee(name=$name, salary=$salary, unusedVacationCount=$unusedVacationCount, usedVacations=$usedVacations)"
+    }
+
+
 }
--- app/src/main/java/camp/nextstep/edu/immutable/ui/EmployeeDatabase.kt
+++ app/src/main/java/camp/nextstep/edu/immutable/ui/EmployeeDatabase.kt
@@ -3,5 +3,16 @@ package camp.nextstep.edu.immutable.ui
 import camp.nextstep.edu.immutable.Employee
 
 object EmployeeDatabase {
-    fun loadEmployees(): List<Employee> = listOf()
+    fun loadEmployees(): List<Employee> = listOf(
+        createEmployee(name = "김태현", salary = Int.MAX_VALUE),
+        createEmployee(name = "김수현", salary = 1_000),
+        createEmployee(name = "제이슨", salary = 100_000_000),
+    )
+
+    private fun createEmployee(name: String, salary: Int): Employee {
+        return Employee().apply {
+            this.name = name
+            this.salary = salary
+        }
+    }
 }
