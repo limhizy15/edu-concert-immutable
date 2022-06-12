1b016ab94bed93ff437d2be227179f8220b688cf
Suhyeon Kim <wisemuji@gmail.com>
2022-06-12 13:16:52 +0900

    Employees 일급 컬렉션을 이용한 필터링 예제 추가

---
+++ app/src/main/java/camp/nextstep/edu/immutable/Employees.kt
@@ -0,0 +1,9 @@
+package camp.nextstep.edu.immutable
+
+data class Employees(val employees: List<Employee>) {
+
+    fun filterOlderThan30(): Employees {
+        TODO()
+    }
+
+}
---
+++ app/src/test/java/camp/nextstep/edu/immutable/EmployeesTest.kt
@@ -0,0 +1,27 @@
+package camp.nextstep.edu.immutable
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class EmployeesTest {
+
+    @Test
+    fun `30세 초과인 사원을 필터링할 수 있어야 한다`() {
+        // given
+        val employees = Employees(
+            listOf(
+                Employee(name = "김", age = 20),
+                Employee(name = "이", age = 32),
+                Employee(name = "강", age = 40),
+            )
+        )
+
+        // when
+        val filteredEmployees = employees.filterOlderThan30()
+
+        // then
+        assertThat(filteredEmployees.employees)
+            .allMatch { it.age > 30 }
+    }
+
+}
