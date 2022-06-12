175f5e946d3242164c5f6e8841a712571d874f8c
Suhyeon Kim <wisemuji@gmail.com>
2022-06-12 15:45:06 +0900

    방어적 복사가 필요한 연봉 테스트 추가

--- app/src/test/java/camp/nextstep/edu/immutable/step2/DefensiveCopyTest.kt
+++ app/src/test/java/camp/nextstep/edu/immutable/step2/DefensiveCopyTest.kt
@@ -5,6 +5,9 @@ import org.junit.Test
 
 class DefensiveCopyTest {
 
+    data class Employee(val name: String, val salary: Int)
+    class Employees(val values: List<Employee>)
+
     @Test
     fun `2개의 리스트는 서로 값이 공유되지 않아야한다`() {
         // given
@@ -18,5 +21,25 @@ class DefensiveCopyTest {
         assertThat(list1).isNotEqualTo(list2)
         assertThat(list1).containsExactly(1, 2, 3, 4)
         assertThat(list2).containsExactly(1, 2, 3)
+    }
+
+    @Test
+    fun `연봉이 바뀌지 않아야 한다`() {
+        // given
+        val employees = Employees(
+            mutableListOf(
+                Employee(name = "김태현", salary = 100_000_000),
+                Employee(name = "김수현", salary = 100_000_000),
+                Employee(name = "제이슨", salary = 100_000_000),
+            )
+        )
+
+        // when
+        (employees.values as MutableList<Employee>)
+            .replaceAll { it.copy(salary = 100) }
+
+        // then
+        assertThat(employees.values)
+            .allMatch { it.salary == 100_000_000 }
     }
 }
