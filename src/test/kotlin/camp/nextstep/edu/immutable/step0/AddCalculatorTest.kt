293a5607b7ab47fc7552423a297792cb9eab8e62
TaeHyun Kim <galcyurio@gmail.com>
2022-07-04 08:05:24 +0900

    강의 시간에 같이 작성하는 step0 테스트 코드를 추가한다

---
+++ src/test/kotlin/camp/nextstep/edu/immutable/step0/AddCalculatorTest.kt
@@ -0,0 +1,15 @@
+package camp.nextstep.edu.immutable.step0
+
+import org.junit.jupiter.api.Test
+
+class AddCalculatorTest {
+    @Test
+    fun `1과 2를 더하면 3이 나와야 한다`() {
+        // given
+
+        // when
+
+        // then
+        TODO("같이 작성해볼거에요 :)")
+    }
+}
